c80ae666ed46e5bb7669fbfce02039e0c46fa1a5
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2018-10-29 15:43:33 -0400

    Modified to example word count project

--- mapper.py
+++ mapper.py
@@ -1,1 +1,8 @@
 # mapper.py
+
+import sys
+
+for line in sys.stdin:
+    words = line.strip().split()
+    for word in words:
+        print('{}\t{}'.format(word, 1))
--- reducer.py
+++ reducer.py
@@ -1,1 +1,24 @@
 # reducer.py
+
+import operator
+import sys
+
+current_word = 0
+current_count = 0
+word = None
+
+for line in sys.stdin:
+    word, count = line.strip().split('\t', 1)
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            print('{}\t{}'.format(current_word, current_count))
+        current_count = count
+        current_word = word
+if current_word == word:
+    print('{}\t{}'.format(current_count, current_count))
