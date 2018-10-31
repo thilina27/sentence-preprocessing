9569d04af15078e9e66db93a02f7a5c7d7569895
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2018-10-30 23:37:55 -0400

    Added custom TextInputFormat

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-timestamp=$(date +%d_%m_%Y_%H%M%S)
+timestamp=$(date +%Y_%m_%d_%H%M%S)
 dir=output/job_$timestamp
 if [ ! -d output ]; then
    mkdir output;
@@ -10,11 +10,13 @@ if [ ! -d output ]; then
 fi
 mkdir $dir
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming-2.6.0-cdh5.13.0.jar \
+	-libjars lib/sent-preproc-format.jar \
 	-file mapper.py \
 	-mapper "/usr/local/bin/python3 mapper.py" \
 	-file reducer.py \
 	-reducer "/usr/local/bin/python3 reducer.py" \
-	-input cs626/project/words.txt \
+	-input cs626/project/$1 \
+	-inputformat cs626.sentence.preprocessing.format.LineNumberInputFormat \
 	-output cs626/project/output
 hadoop fs -copyToLocal cs626/project/output/* $dir/
 hadoop fs -rm -r cs626/project/output/
