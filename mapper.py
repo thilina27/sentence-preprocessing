ce39f037f10cc219b2358e842aa7bc8e874c07cf
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2018-10-30 23:38:31 -0400

    Modified MapReduce to output blocks of sentences with their order number

--- mapper.py
+++ mapper.py
@@ -2,7 +2,42 @@
 
 import sys
 
+SENTENCE_ENDINGS = ['.', '!', '?', '"']
+ABBREVIATIONS = ['mr.', 'mrs.', 'ms.', 'jr.', 'sr.', 'dr.']
+
+block_count = 0
+curr_block = []
+
 for line in sys.stdin:
-    words = line.strip().split()
-    for word in words:
-        print('{}\t{}'.format(word, 1))
+    line = ' '.join(line.strip().split()[1:]).lower()
+    if len(line) == 0:
+        # Skip empty lines, TODO: something else here?
+        continue
+    in_single_sentence = False
+    if len(curr_block) == 0:
+        curr_block = [line]
+        in_single_sentence = True
+    elif line[0] == '"':
+        # End the previous block
+        print('{}\t{}'.format(block_count, ' '.join(curr_block)))
+        curr_block = [line]
+        block_count += 1
+        in_single_sentence = True
+    # Check if this line ends a sentence
+    if line[-1] in SENTENCE_ENDINGS:
+        # Check that it's not a period and one of our abbreviations
+        if line[-1] == '.' and line.split()[-1] in ABBREVIATIONS:
+            # Remove the period
+            line = line[:-1]
+            # Add line to block and continue to next iteration
+            curr_block.append(line)
+            continue
+        # End the current block
+        if not in_single_sentence:
+            # Because we've already added this line to curr_block
+            curr_block.append(line)
+        print('{}\t{}'.format(block_count, ' '.join(curr_block)))
+        curr_block = []
+        block_count += 1
+    elif not in_single_sentence:
+        curr_block.append(line)
--- reducer.py
+++ reducer.py
@@ -3,22 +3,12 @@
 import operator
 import sys
 
-current_word = 0
-current_count = 0
-word = None
+SENTENCE_ENDINGS = ['.', '!', '?', '"']
+
+sentence_count = 0
+current_sentence = []
 
 for line in sys.stdin:
-    word, count = line.strip().split('\t', 1)
-    try:
-        count = int(count)
-    except ValueError:
-        continue
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            print('{}\t{}'.format(current_word, current_count))
-        current_count = count
-        current_word = word
-if current_word == word:
-    print('{}\t{}'.format(current_word, current_count))
+    block_num, block = line.strip().split('\t',1)
+    print('{}\t{}'.format(block_num, block))
+
